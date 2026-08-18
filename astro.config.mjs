778726813a7750ebe9e90ede484368ae8aa4dc4d
tatara42 <murasakitatara@gmail.com>
2026-08-19 02:25:14 +0530

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+  site: 'https://tatara42.github.io',
+  base: '/',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
